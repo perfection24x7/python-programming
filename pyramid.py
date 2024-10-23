756a4547a6f2442f3bd43afd4871b049fbf44773
Naveen Kumar Jain <perfection24x7@gmail.com>
2024-10-24 02:43:54 +0530

    Create pyramid.py

---
+++ pyramid.py
@@ -0,0 +1,15 @@
+x = int(input("Please enter digit"))
+
+for i in range(int(x), 0, -1):
+    print('* ' * i)
+  
+#  Output
+#  Please enter digit
+
+#  I entered 5 then output will be:
+
+#  *  *  *  *  *
+#  *  *  *  *
+#  *  *  *
+#  *  *  
+#  *
